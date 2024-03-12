7b1ed08cb518dcd78cf6275da8a39afff42325c9
CGU <Mohameddeaf65@gmail.com>
2024-03-12 14:01:25 +0200

    0-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+Design a method that determines if all the boxes can be opened.
+"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    if len(keys) == len(boxes):
+        return True
+    return False

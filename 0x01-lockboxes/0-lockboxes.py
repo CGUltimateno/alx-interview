a0b27c725663266fac9350049a52f510251fdbae
CGU <Mohameddeaf65@gmail.com>
2024-03-12 14:15:14 +0200

    0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
-Design a method that determines if all the boxes can be opened.
-"""
+"""You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box
+may contain keys to the other boxes. Design a method that determines if all the boxes can be opened."""
 
 
 def canUnlockAll(boxes):

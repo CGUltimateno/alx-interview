5c76070ffe11ceb0e5c4aaaae717df51df7ea9f1
CGUltimateno <Mohameddeaf65@gmail.com>
2024-03-28 18:03:16 +0200

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,38 +2,63 @@
 """
 Write a script that reads stdin line by line and computes metrics
 """
+import random
 import sys
+from time import sleep
+import datetime
+
 
 def print_stats(file_size, status_codes):
     """
     Print the statistics
     """
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        print("{}: {}".format(key, value))
+    for i in range(10000):
+        sleep(random.random())
+        sys.stdout.write(
+            "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n"
+            .format(
+                random.randint(1, 255), random.randint(1, 255),
+                random.randint(1, 255), random.randint(1, 255),
+                datetime.datetime.now(),
+                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+                random.randint(1, 1024)))
+        sys.stdout.flush()
+
 
 def main():
     """
     Main function
     """
     file_size = 0
-    status_codes = {}
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     count = 0
+    stats = {k: 0 for k in status_codes}
+
+    def print_statistics():
+        print("File size: {:d}".format(file_size))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
     try:
         for line in sys.stdin:
             count += 1
             data = line.split()
-            file_size += int(data[-1])
-            status_code = data[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            else:
-                status_codes[status_code] = 1
-            if count == 10:
-                print_stats(file_size, status_codes)
-                count = 0
+            try:
+                sc = data[-2]
+                if sc in stats:
+                    stats[sc] += 1
+            except (IndexError, KeyError):
+                pass
+            try:
+                file_size += int(data[-1])
+            except (TypeError, IndexError, ValueError):
+                pass
+            if count % 10 == 0:
+                print_statistics()
+        print_statistics()
     except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
+        print_statistics()
         raise
 
 

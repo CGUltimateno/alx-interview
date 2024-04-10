b45b87a0b4225559723b2031f7c1a373b4f4833d
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-10 22:28:54 +0200

    0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,12 +3,43 @@
 import sys
 
 
-def is_safe(board, row, col, n):
-    """The N queens puzzle is the challenge of placing N
-    non-attacking queens on an N×N chessboard.
-    Write a program that solves the N queens problem"""
+def is_safe(board, row, col):
+    """
+  Checks if a queen can be placed at the given row and column without attacking other queens.
+  """
     for i in range(col):
-        if board[i] == row or board[i] == row - col + i or board[i] == row + col - i:
+        if board[row][i] == 1:
             return False
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i, j = row, col
+    while i < len(board) and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
     return True
 
+
+def solve_n_queens(board, col):
+    """
+  Solves the N-queens problem using backtracking.
+  """
+    if col >= len(board):
+        for row in board:
+            print("".join(['Q' if x == 1 else '.' for x in row]))
+        return
+
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            solve_n_queens(board, col + 1)
+            board[i][col] = 0

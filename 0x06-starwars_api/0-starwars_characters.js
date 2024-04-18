5c24177ff5677bdddb04268485d63a8bdbc444f3
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-18 11:17:57 +0200

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,23 +6,30 @@ const request = require('request');
 const { argv } = require('process');
 let url = " ";
 
-if (process.argv.length === 3){
-   url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
-    request(url, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            let characters = JSON.parse(body).characters;
-            for (let i = 0; i < characters.length; i++) {
-                request(characters[i], function (error, response, body) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log(JSON.parse(body).name);
-                    }
-                });
-            }
-        }
+if (argv.length === 3) {
+  url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
     }
-    );
+
+    getCharacter(JSON.parse(body).characters, 0);
+  });
+}
+
+function getCharacter (characters, i) {
+    if (i === characters.length) {
+        return;
+    }
+
+    request(characters[i], (error, response, body) => {
+        if (error) {
+        console.error(error);
+        return;
+        }
+
+        console.log(JSON.parse(body).name);
+        getCharacter(characters, i + 1);
+    });
 }

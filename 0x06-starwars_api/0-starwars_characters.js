0aee427ed86b93bff6c801708e12dd1469475f06
CGUltimateno <Mohameddeaf65@gmail.com>
2024-04-18 11:17:11 +0200

    0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,24 +3,26 @@
  * This script prints all characters of a Star Wars movie
  */
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, async function (error, response, body) {
-    if (error) {
-        console.error(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-            await new Promise((resolve, reject) => {
-                request(character, function (error, response, body) {
+const { argv } = require('process');
+let url = " ";
+
+if (process.argv.length === 3){
+   url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+    request(url, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            let characters = JSON.parse(body).characters;
+            for (let i = 0; i < characters.length; i++) {
+                request(characters[i], function (error, response, body) {
                     if (error) {
-                        console.error(error);
+                        console.log(error);
                     } else {
                         console.log(JSON.parse(body).name);
                     }
-                    resolve();
                 });
-            });
+            }
         }
     }
+    );
 }
-);

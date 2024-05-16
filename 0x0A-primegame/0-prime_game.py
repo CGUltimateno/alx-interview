36f949ef710f9d5ad75ab18b0109edf05cc9b77e
CGUltimateno <Mohameddeaf65@gmail.com>
2024-05-17 02:18:21 +0300

    try 2

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
 """Prime Game."""
 
+
 def is_prime(n):
     """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
 
 
 def prime_game(n, m):
     """Determine who wins the game."""
-    if n == 0 or n == 1:
+    if n is None or m is None or n == 0 or m == []:
         return None
-    if m == 0:
-        return None
-    if n == 2:
-        return "Ben"
-    if n == 3:
-        return "Maria"
-    if m == 1:
-        return "Maria"
-    ben = 0
-    maria = 0
-    for i in range(n, m + 1):
-        if is_prime(i):
-            ben += 1
+    Maria = Ben = 0
+    for i in range(n):
+        primeNos = is_prime(m[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
         else:
-            maria += 1
-    if ben > maria:
-        return "Ben"
-    if maria > ben:
-        return "Maria"
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
     return None

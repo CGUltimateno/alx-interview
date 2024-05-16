acdc60840e14edc18e88494321a794f6275e0805
CGUltimateno <Mohameddeaf65@gmail.com>
2024-05-17 02:15:34 +0300

    0x0A-primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,3 +15,29 @@ def is_prime(n):
             return False
         i += 6
     return True
+
+
+def prime_game(n, m):
+    """Determine who wins the game."""
+    if n == 0 or n == 1:
+        return None
+    if m == 0:
+        return None
+    if n == 2:
+        return "Ben"
+    if n == 3:
+        return "Maria"
+    if m == 1:
+        return "Maria"
+    ben = 0
+    maria = 0
+    for i in range(n, m + 1):
+        if is_prime(i):
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
